993945db4507cd85792871bb30516885ff174705
SumitNalavade <vs.nalavade2003@gmail.com>
2022-06-29 15:13:46 -0500

    Refactor getProjects

--- controller/github.ts
+++ controller/github.ts
@@ -1,40 +1,35 @@
 import axios from "axios";
 import { Marked } from "@ts-stack/markdown";
-import { IProject } from "../utils/Project";
 
 export default async function getProjects() {
-    const repos: [any]  = (await axios.get("https://api.github.com/users/SumitNalavade/repos?sort=created")).data;
+    /*
+        Get all of the repos from github,
+        Loop through each repo and get the README,
+        Filter out just the image URL in the README,
+        Add the imageURL to the final object
+    */
 
-    const projects: IProject[] = repos.map((repo: any) => {
+    const projects = (await axios.get("https://api.github.com/users/SumitNalavade/repos?sort=created")).data.map(async (repo: any) => {
+        let imagePath: string = "";
+
+        try {
+            const readme = (await axios.get(`https://raw.githubusercontent.com/SumitNalavade/${repo.name}/${repo.default_branch}/README.md`)).data;
+            imagePath = (Marked.parse(readme).match(/<img[^>]+src="([^">]+)"/)![1])
+        } catch {
+            imagePath = "https://pngimg.com/uploads/github/github_PNG15.png"
+        }
+
         return {
             title: repo.name,
             description: repo.description,
             url: repo.url,
-            default_branch: repo.default_branch
-        }
-    })
-
-    return projects;
-};
-
-export async function getImages() {
-    const projects = await getProjects();
-    let readMes = projects.map(async (project: IProject) => {
-        try {
-            return await (await axios.get(`https://raw.githubusercontent.com/SumitNalavade/${project.title}/${project.default_branch}/README.md`)).data
-        } catch {
-            return "Temp"
+            default_branch: repo.default_branch,
+            imagePath: imagePath + "?raw=true"
         }
     });
 
-    const settledReadMes = await Promise.allSettled(readMes);
-    settledReadMes.map((readme: any) => {
-        readme.value = Marked.parse(readme.value).match(/<img[^>]+src="([^">]+)"/);
+    // @ts-ignore
+    const result = (await Promise.allSettled(projects)).map(({ status, value }) => value);
 
-        if(!readme.value) {
-            readme.value = ["https://pngimg.com/uploads/github/github_PNG15.png", "https://pngimg.com/uploads/github/github_PNG15.png"]
-        }
-    });
-
-    return settledReadMes;
+    return result;
 };
--- pages/work.tsx
+++ pages/work.tsx
@@ -4,22 +4,18 @@ import Image from "next/image";
 
 import Navbar from '../components/Navbar';
 import SectionHeader from '../components/SectionHeader';
-import getProjects, { getImages } from '../controller/github';
+import getProjects from '../controller/github';
 import { IProject } from "../utils/Project";
 
 interface Props {
     projects: [IProject]
-    projectImages: [string]
 }
 
-const Work: NextPage<Props> = ({ projects, projectImages }) => {
-    console.log(projectImages);
-
+const Work: NextPage<Props> = ({ projects }) => {
     return (
         <div className="h-100">
             <Navbar />
             <div className='d-flex justify-content-center'><SectionHeader title='Projects' /></div>
-
             <section className="bg-background pt-5 pb-5 shadow-sm">
                 <div className="container">
                     <div className="row">
@@ -28,7 +24,7 @@ const Work: NextPage<Props> = ({ projects, projectImages }) => {
                         return (
                             <div key={index} className="col-lg-4 mb-3 d-flex align-items-stretch">
                                 <div className="card">
-                                    <img src={projectImages[index].value[1] + "?raw=true"} className="card-img-top" alt="Card Image" />
+                                    <img src={project.imagePath} className="card-img-top" alt="Card Image" />
                                     <div className="card-body d-flex flex-column">
                                         <h5 className="card-title">{project.title}</h5>
                                         <p className="card-text mb-4">{project.description}</p>
@@ -48,9 +44,8 @@ const Work: NextPage<Props> = ({ projects, projectImages }) => {
 
 export const getServerSideProps: GetServerSideProps = async() => {
     const projects = await getProjects();
-    const projectImages = await getImages();
 
-    return { props: { projects, projectImages } }
+    return { props: { projects } };
 }
 
 export default Work;

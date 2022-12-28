a9a1b8f8e35c75dc3aa8cfd9da2dc745eaba783a
SumitNalavade <vs.nalavade2003@gmail.com>
2022-12-27 19:05:58 -0600

    Update Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
-import { Box, Button, Container, Flex, Heading, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Text, SimpleGrid } from '@chakra-ui/react'
 
 import Navbar from '../components/Navbar'
 
 export default function Home() {
+  const myLoader = () => {
+    return `https://avatars.githubusercontent.com/u/48499839?v=4`
+  }
+
   return (
     <>
       <Head>
@@ -16,27 +21,33 @@ export default function Home() {
 
       <Navbar />
 
-      <Flex flexDirection={"column"} justifyContent="center" m={"auto"} maxW="container.xl" h="75vh">
-        <Heading size="sm" color="brand.headline" mb={4} ml={2}>Hi, my name is</Heading>
-        <Heading size="4xl" color="brand.headline">Sumit Nalavade.</Heading>
+      <Flex p={4} justifyContent="space-between" flexWrap={"wrap"} m={"auto"} maxW="container.xl" h="75vh">
+        <Flex flexDirection={"column"} justifyContent="center">
+          <Heading size="sm" color="brand.headline" mb={4} ml={2}>Hi, my name is</Heading>
+          <Heading size="4xl" color="brand.headline">Sumit Nalavade.</Heading>
 
-        <Box color="brand.paragraph" mt={8} maxW={"3xl"}>
-          <Text my={2}>
-            I'm an engineering student at Texas A&M University '26 !
-          </Text>
+          <Box color="brand.paragraph" mt={8} maxW={"3xl"}>
+            <Text my={2}>
+              I'm an engineering student at Texas A&M University '26 !
+            </Text>
 
-          <Text my={2} noOfLines={2}>
-            My interests include software design and development, machine learning and artificial intelligence.
-          </Text>
+            <Text my={2} noOfLines={2}>
+              My interests include software design and development, machine learning and artificial intelligence.
+            </Text>
 
-          <Text my={2} noOfLines={2}>
-            Currently, I'm focused on earning my bachelors degree in Computer Engineering on the Computer Science track.
-          </Text>
-        </Box>
+            <Text my={2} noOfLines={2}>
+              Currently, I'm focused on earning my bachelors degree in Computer Engineering on the Computer Science track.
+            </Text>
+          </Box>
 
-        <Button color="brand.tertiary" borderColor="brand.tertiary" variant='outline' mr={2} mt={8} w="2xs">
-          Check out my work!
-        </Button>
+          <Button color="brand.tertiary" borderColor="brand.tertiary" variant='outline' mr={2} mt={8} w="2xs" _hover={{ bg: '#fffafb' }}>
+            Check out my work!
+          </Button>
+        </Flex>
+
+        <Flex mt={4} flexDirection={"column"} justifyContent="center" w={"sm"}>
+          <Image loader={myLoader} src="me.png" alt='Image of myself' height={50} width={50} layout="responsive"  />
+        </Flex>
       </Flex>
     </>
   )

f3f8289e1c796462aff302be0044a9fe68c57d4e
SumitNalavade <vs.nalavade2003@gmail.com>
2023-01-11 19:49:48 -0600

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,34 +12,7 @@ const Home: NextPage = () => {
   const { width, height } = useWindowSize();
 
   return (
-    <Layout>
-        <Flex flexDirection={"column"} justifyContent="center">
-          <Heading size="sm" color="brand.headline" mb={2} ml={2}>Hi, my name is</Heading>
-          <Heading size="4xl" color="brand.headline">Sumit Nalavade.</Heading>
-
-          <Box color="brand.paragraph" mt={8} maxW={"3xl"}>
-            <Text my={2}>
-              I'm an engineering student at Texas A&M University '26 !
-            </Text>
-
-            <Text my={2} noOfLines={2}>
-              My interests include software design and development, machine learning and artificial intelligence.
-            </Text>
-
-            <Text my={2} noOfLines={2}>
-              Currently, I'm focused on earning my bachelors degree in Computer Engineering on the Computer Science track.
-            </Text>
-          </Box>
-
-          <Button color="brand.tertiary" borderColor="brand.tertiary" variant='outline' mr={2} mt={8} mb={4} w="2xs" _hover={{ bg: '#fffafb' }}>
-            <Link href="/projects">Check out my work!</Link>
-          </Button>
-        </Flex>
-
-        <Flex my={4} flexDirection={"column"} justifyContent="center" w={"sm"} order={ width < 1184 ? "-1" : "1" } >
-          <Image loader={() => `https://avatars.githubusercontent.com/u/48499839?v=4`} src="me.png" alt='Image of myself' height={50} width={50} layout="responsive"  />
-        </Flex>
-    </Layout>
+   <h1>Nfdfsdfdfsfd</h1>
   )
 }
 

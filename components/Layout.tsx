54eed82cfb6cd894718edd4cb3f9ba0c5cf685af
SumitNalavade <vs.nalavade2003@gmail.com>
2023-06-09 10:36:59 -0500

    Update meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 
 import Navbar from "./Navbar";
 
@@ -10,16 +9,6 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <Head>
-        <title>Sumit Nalavade - Portfolio</title>
-        <meta
-          name="description"
-          content="Sumit Nalavade - Texas A&M Engineering"
-        ></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/icon.png" />
-      </Head>
-
       <Navbar />
 
       <div className="p-4 flex justify-between flex-wrap mx-auto max-w-[1400px] h-[75vh]">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Sumit Nalavade - Portfolio</title>
+        <meta name="description" content="Sumit Nalavade - Texas A&M Engineering"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Sumit Nalavade - Portfolio" key="title" />
+        <meta
+          property="og:description"
+          content="I'm a computer engineering student at Texas A&M University '26! My interests include software design and development, machine learning and artificial intelligence."
+          key="description"
+        />
+        <meta property="og:image" content="https://avatars.githubusercontent.com/u/48499839?v=4" key="description" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

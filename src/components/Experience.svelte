3fa4a467a111dca0af7af0a9ac96d5c444b9a959
SumitNalavade <vs.nalavade2003@gmail.com>
2023-07-07 03:45:27 -0500

    Add experience sidepanel

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -10,6 +10,8 @@
     employer: string;
     employerImage: string;
     location: string;
+    start: string,
+    end?: string,
     duration: string;
     description: string;
   }
@@ -21,14 +23,21 @@
       employerImage: TAMUHealthLogo,
       location: "Bryan, Texas",
       duration: "Present",
-      description: `Contributed to the development of OliviaHealth, a grant funded project lead by the Texas A&M Health Science Center to alleviate maternal and familial care.
-        Developed a highly intuitive user interface using React.js and TypeScript, empowering users to effortlessly customize preferences and tailor the output to their specific needs.
-        Integrated an SBERT machine learning model into an API to enable responsive handling of user queries, akin to advanced language models like Chat-GPT and Google Bard.
-        Expanded the algorithm's scope by incorporating a broader range of parameters, including user preferences, historical data, and contextual information, to enhance the relevance of the generated output
-        Focused on building a resilient and scalable backend architecture with Python and Flask, facilitating seamless integration with the frontend. 
-        Streamlined data flow, ensuring data consistency and establishing reliable communication channels between the frontend and backend systems.
-        Optimized data flow, ensuring data consistency and reliable communication channels between font and backend systems.
-        Continuously gathered valuable user feedback and conducted extensive usability testing, utilizing. libraries such as Jest and Selenium.`,
+      start: "June, 2023",
+      description: `
+Contributed to the development of OliviaHealth, a grant funded project lead by the Texas A&M Health Science Center to alleviate maternal and familial care.
+
+Developed a highly intuitive user interface using React.js and TypeScript, empowering users to effortlessly customize preferences and tailor the output to their specific needs.
+
+Integrated an SBERT machine learning model into an API to enable responsive handling of user queries, akin to advanced language models like Chat-GPT and Google Bard.
+
+Expanded the algorithm's scope by incorporating a broader range of parameters, including user preferences, historical data, and contextual information, to enhance the relevance of the generated output.
+
+Focused on building a resilient and scalable backend architecture with Python and Flask, facilitating seamless integration with the frontend. 
+
+Streamlined data flow, ensuring data consistency and establishing reliable communication channels between the frontend and backend systems.
+
+Continuously gathered valuable user feedback and conducted extensive usability testing, utilizing. libraries such as Jest and Selenium.`,
     },
     {
       title: "Academic Peer Mentor",
@@ -36,25 +45,38 @@
       employerImage: TAMULogo,
       location: "College Station, Texas",
       duration: "Present",
-      description: `Provided regular, one-on-one peer academic coaching to students who live on campus
-        Raised academic expectations with engagement and compassion through programs and services
-        Serving as a resource to connect students with programs and initiatives that enhance learning and success
-        Partnering with Resident Advisors to foster campus community and feelings of belonging on-campus
-        Offer opportunities for academic and personal development within the residence halls
-        Held weekly office hours to support students’ academic and personal goals`,
+      start: "June, 2023",
+      description: `
+Provided regular, one-on-one peer academic coaching to students who live on campus.
+
+Raised academic expectations with engagement and compassion through programs and services.
+
+Serving as a resource to connect students with programs and initiatives that enhance learning and success.
+
+Partnered with Resident Advisors to foster campus community and feelings of belonging on-campus.
+
+Offered opportunities for academic and personal development within the residence halls
+
+Held weekly office hours to support students’ academic and personal goals`,
     },
     {
       title: "Project Manager/Lead Developer",
       employer: "Aggie Coding Club",
       employerImage: ACCLogo,
       location: "College Station, Texas",
+      start: "August, 2022",
+      end: "May, 2023",
       duration: "8 Months",
       description: `
-            Led a team of 20+ students to create a cross platform mobile app at Texas A&M University to connect new students with similar interests.
-            Implemented mobile technologies including React Native and TypeScript and backend technologies including Node.js, Express.js and Google Firebase.
-            Created a REST API to relay information from frontend client to backend server to Google Firestore database.
-            Taught team members how to use Git, React Native, TypeScript, Firebase etc…
-            Held weekly stand-up meetings to discuss progress, update team on goals and held mini-lessons covering various technologies.Led a team of 20+ students to create a cross platform mobile app at Texas A&M University to connect new students with similar interests. Implemented mobile technologies including React Native and TypeScript and backend technologies including Node.js, Express.js and Google Firebase. Created a REST API to relay information from frontend client to backend server to Google Firestore database. Taught team members how to use Git, React Native, TypeScript, Firebase etc… Held weekly stand-up meetings to discuss progress, update team on goals and held mini-lessons covering various technologies.
+Led a team of 20+ students to create a cross platform mobile app at Texas A&M University to connect new students with similar interests.
+
+Implemented mobile technologies including React Native and TypeScript and backend technologies including Node.js, Express.js and Google Firebase.
+
+Created a REST API to relay information from frontend client to backend server to Google Firestore database.
+
+Taught team members how to use Git, React Native, TypeScript, Firebase etc…
+
+Held weekly stand-up meetings to discuss progress, update team on goals and held mini-lessons covering various technologies.
     `,
     },
     {
@@ -63,10 +85,16 @@
       employerImage: CryptLogo,
       location: "Austin, Texas",
       duration: "3 Months",
-      description: `Designed and developed Crypt’s front-end web application to securely encrypt, store and retrieve user data on the IPFS network using SHA-256 and AES block cipher.
-            Used modern technologies including TypeScript, Next.js, React.js and Tailwind.
-            Replaced Create-React-App build tooling on the existing codebase to Next.js static site generation to greatly improve performance, SEO and page insights.
-            Documented and tested codebase before releasing to the company.`,
+      start: "June, 2022",
+      end: "Auguest, 2022",
+      description: `
+Designed and developed Crypt’s front-end web application to securely encrypt, store and retrieve user data on the IPFS network using SHA-256 and AES block cipher.
+
+Used modern technologies including TypeScript, Next.js, React.js and Tailwind.
+
+Replaced Create-React-App build tooling on the existing codebase to Next.js static site generation to greatly improve performance, SEO and page insights.
+
+Documented and tested codebase before releasing to the company.`,
     },
     {
       title: "Student Journalist",
@@ -74,19 +102,24 @@
       employerImage: CNTVLogo,
       location: "Frisco, Texas",
       duration: "3 Years",
+      start: "August, 2019",
+      end: "May, 2022",
       description: `As a student journalist at Coyote Nation TV, I work with a small team of passionate creators to maintain Heritage High School's broadcasting program, a media outlet meant to educate, inform and entertain the students of HHS. In my three years in this program so far I've been able to create dozens of stories and segments tailored to tell the stories of students and offer an insight into the dynamic world of a High School student's life.`,
     },
   ];
+
+  let focusedExperience = experiences[0];
 </script>
 
 <main>
   <h2 class="text-2xl font-semibold mb-2 ml-2">Experience</h2>
 
-  <div class="flex">
+  <div class="flex gap-12">
     <div class="w-2/3">
-      {#each experiences as experience}
-        <div
-          class="flex items-center gap-4 w-full text-sm p-2 my-4 rounded-xl hover:bg-secondary"
+      {#each experiences as experience, index}
+        <button
+          class={`flex items-center gap-4 w-full text-left text-sm p-2 my-4 rounded-xl hover:bg-secondary ${ focusedExperience === experience ? "text-primary" : "" }`}
+          on:click={() => focusedExperience = experiences[index]}
         >
           <img
             src={experience.employerImage}
@@ -96,10 +129,30 @@
           <p class="w-1/2">{experience.title}</p>
           <p class="w-1/4">{experience.duration}</p>
           <p class="w-1/4">{experience.location}</p>
-        </div>
+        </button>
       {/each}
     </div>
 
-    <div class="w-1/2" />
+    <div class="w-1/2">
+        <div class="flex items-end gap-4 mb-4">
+            <img src={focusedExperience.employerImage} class="rounded-xl w-24" alt={`${focusedExperience.employer} Logo`}>
+            <div>
+                <p class="font-semibold text-xl">{ focusedExperience.title }</p>
+                <p class="my-1">
+                    { focusedExperience.start } -
+                    { #if focusedExperience.end}
+                        <span>{focusedExperience.end}</span>
+                        {:else}
+                        <span>Present</span>
+                    {/if }
+                    
+                </p>
+                <p>{ focusedExperience.location }</p>
+            </div>
+        </div>
+        <div class="whitespace-pre-line">
+            { focusedExperience.description }
+        </div>
+    </div>
   </div>
 </main>

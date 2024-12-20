bcc168debd15026fe018d217fcf298df76b596b2
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-12-20 14:47:10 -0600

    Add projects to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 import { FiLinkedin, FiGithub, FiInstagram } from "react-icons/fi";
+import { FaReact, FaApple, FaAndroid, FaJsSquare, FaNpm } from "react-icons/fa";
+import { BiLogoTypescript, BiLogoTailwindCss, BiLogoGoogleCloud, BiLogoNodejs } from "react-icons/bi";
+import { VscVscode } from "react-icons/vsc";
 
 export default function Home() {
   return (
@@ -23,30 +26,30 @@ export default function Home() {
           <p>About Me</p>
         </a>
 
-        <a href="" className='border border-1 border-white px-4 py-2 rounded-full flex items-center gap-x-2'>
+        <a href="" className='border border-1 border-white px-4 py-2 rounded-full flex items-center gap-x-2 hover:bg-white hover:text-black'>
           <FiLinkedin />
           <p>LinkedIn</p>
         </a>
 
-        <a href="" className='border border-1 border-white px-4 py-2 rounded-full flex items-center gap-x-2'>
+        <a href="" className='border border-1 border-white px-4 py-2 rounded-full flex items-center gap-x-2 hover:bg-white hover:text-black'>
           <FiGithub />
           <p>GitHub</p>
         </a>
 
-        <a href="" className='border border-1 border-white px-4 py-2 rounded-full flex items-center gap-x-2'>
+        <a href="" className='border border-1 border-white px-4 py-2 rounded-full flex items-center gap-x-2 hover:bg-white hover:text-black'>
           <FiInstagram />
           <p>Instagram</p>
         </a>
       </div>
 
-      <div className='px-32 text-white space-y-4'>
+      <div className='px-32 text-white space-y-4 text-sm'>
         <p className='text-4xl font-bold pb-4'>Experience</p>
 
         <div className='flex items-center justify-between gap-x-8'>
           <Image
             src="/chase_logo.png"
             alt="Example"
-            width={50} // Aspect ratio width
+            width={40} // Aspect ratio width
             height={9} // Aspect ratio height
           />
 
@@ -67,7 +70,7 @@ export default function Home() {
           <Image
             src="/humana_logo.png"
             alt="Example"
-            width={50} // Aspect ratio width
+            width={40} // Aspect ratio width
             height={9} // Aspect ratio height
           />
 
@@ -88,7 +91,7 @@ export default function Home() {
           <Image
             src="/tamu_hsc_logo.png"
             alt="Example"
-            width={50} // Aspect ratio width
+            width={40} // Aspect ratio width
             height={9} // Aspect ratio height
           />
 
@@ -109,7 +112,7 @@ export default function Home() {
           <Image
             src="/tamu_coe_logo.png"
             alt="Example"
-            width={50} // Aspect ratio width
+            width={40} // Aspect ratio width
             height={9} // Aspect ratio height
           />
 
@@ -127,7 +130,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='px-32 text-white space-y-4'>
+      <div className='px-32 text-white space-y-4 text-sm'>
         <div className='pb-4 space-y-2'>
           <p className='text-4xl font-bold'>Projects</p>
           <p>A collection of notable projects I've built from web to mobile apps and everything in between.</p>
@@ -136,55 +139,104 @@ export default function Home() {
         <div className='flex gap-x-6'>
           <div className='bg-[#292A2A] rounded-3xl p-5 space-y-2'>
             <div className='w-44 h-44'>
-              <div className='bg-[#D9D9D9] w-full h-full rounded-2xl'>
-
-              </div>
+              <Image
+                src="/maroon_rides_demo.png"
+                alt="Example"
+                layout="responsive"
+                width={40} // Aspect ratio width
+                height={9} // Aspect ratio height
+              />
             </div>
 
-            <div className='px-1'>
-              <p className='font-bold'>Project Header</p>
-              <p>Subtitle</p>
+            <div className='px-1 space-y-1'>
+              <p className='font-bold text-base'>Maroon Rides</p>
+              <div className='flex gap-x-2 items-center'>
+                <FaApple />
+                <FaAndroid />
+                <FaReact />
+                <FaJsSquare />
+                <BiLogoTypescript />
+                <BiLogoNodejs />
+                <FaNpm />
+              </div>
             </div>
           </div>
 
-          <div className='rounded-3xl p-5 space-y-2'>
-            <div className='w-44 h-44'>
-              <div className='bg-[#D9D9D9] w-full h-full rounded-2xl'>
-
-              </div>
+          <div className='bg-[#292A2A] rounded-3xl p-5 space-y-2'>
+            <div className='w-44 h-44 flex justify-center items-center'>
+              <Image
+                src="/ichild_demo.png"
+                alt="Example"
+                layout="responsive"
+                width={40} // Aspect ratio width
+                height={9} // Aspect ratio height
+              />
             </div>
 
-            <div className='px-1'>
-              <p className='font-bold'>Project Header</p>
-              <p>Subtitle</p>
+            <div className='px-1 space-y-1'>
+              <p className='font-bold text-base'>intelligentCHILD</p>
+              <div className='flex gap-x-2 items-center'>
+                <FaReact />
+                <FaJsSquare />
+                <BiLogoTypescript />
+                <BiLogoTailwindCss />
+                <BiLogoGoogleCloud />
+              </div>
             </div>
           </div>
 
-          <div className='rounded-3xl p-5 space-y-2'>
-            <div className='w-44 h-44'>
-              <div className='bg-[#D9D9D9] w-full h-full rounded-2xl'>
-
-              </div>
+          <div className='bg-[#292A2A] rounded-3xl p-5 space-y-2'>
+            <div className='w-44 h-44 flex justify-center items-center'>
+              <Image
+                src="/gradual_demo.png"
+                alt="Example"
+                layout="responsive"
+                width={40} // Aspect ratio width
+                height={9} // Aspect ratio height
+              />
             </div>
 
-            <div className='px-1'>
-              <p className='font-bold'>Project Header</p>
-              <p>Subtitle</p>
+            <div className='px-1 space-y-1'>
+              <p className='font-bold text-base'>Gradual Grades</p>
+              <div className='flex gap-x-2 items-center'>
+                <FaApple />
+                <FaAndroid />
+                <FaReact />
+                <FaJsSquare />
+                <BiLogoNodejs />
+              </div>
             </div>
           </div>
 
-          <div className='rounded-3xl p-5 space-y-2'>
-            <div className='w-44 h-44'>
-              <div className='bg-[#D9D9D9] w-full h-full rounded-2xl'>
+          <div className='bg-[#292A2A] rounded-3xl p-5 space-y-2'>
+            <div className='w-44 h-44 flex justify-center items-center'>
+              <Image
+                src="/vs_code_readme_editor_demo.png"
+                alt="Example"
+                layout="responsive"
+                width={40} // Aspect ratio width
+                height={9} // Aspect ratio height
+              />
+            </div>
 
+            <div className='px-1 space-y-1'>
+              <p className='font-bold text-base'>VS Code ReadMe Editor</p>
+              <div className='flex gap-x-2 items-center'>
+                <VscVscode />
+                <FaReact />
+                <FaJsSquare />
+                <BiLogoTypescript />
               </div>
             </div>
-
-            <div className='px-1'>
-              <p className='font-bold'>Project Header</p>
-              <p>Subtitle</p>
-            </div>
           </div>
+        </div>
+      </div>
+
+      <div className='px-20 text-white space-y-4 py-10'>
+        <div className='pb-4 space-y-4 px-12 py-6 bg-[#292A2A] rounded-3xl'>
+          <p className='text-4xl font-bold'>About</p>
+          <p>Howdy! I am a computer engineering student at Texas A&M University with a focus on computer science. With a passion for building software, I have hands on experience in modern technologies including full-stack and cross-platform development on web and mobile platforms.</p>
+          <p>Currently, I am looking forward to starting my second software engineering internship at JP Morgan Chase while contiuing my software engineering co-op at Humana!</p>
         </div>
       </div>
 

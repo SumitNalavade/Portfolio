4c117aa39739c2c048f1205f83fe2853c2c01a68
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-01-11 16:09:18 -0600

    Fix deployment bugs on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,26 +4,6 @@ import Image from 'next/image';
 import { FaRegEnvelope, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Home() {
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
-
     return (
         <div className='h-full space-y-6 sm:space-y-12'>
             <div

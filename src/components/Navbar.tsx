6c7bc71027e8c9f8441a0dc4c81096fe333c1d3d
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-10-22 18:19:41 -0500

    Make navbar links Next links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { usePathname } from 'next/navigation'
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
     const pathname = usePathname()
@@ -9,9 +10,9 @@ const Navbar: React.FC = () => {
     return (
         <div className="py-8 flex justify-end-safe items-center">
             <ul className="flex gap-x-5">
-                <li className={`${pathname === '/' ? 'underline' : ''}`}>About</li>
-                <li className={`${pathname.includes('/work') ? 'underline' : ''}`}>Work</li>
-                <li className={`${pathname.includes('/blog') ? 'underline' : ''}`}>Blog</li>
+                <li className={`${pathname === '/' ? 'underline' : ''}`}><Link href={'/'}>About</Link></li>
+                <li className={`${pathname.includes('/work') ? 'underline' : ''}`}><Link href={'/work'}>Work</Link></li>
+                <li className={`${pathname.includes('/blog') ? 'underline' : ''}`}><Link href={'/blog'}>Blog</Link></li>
             </ul>
         </div>
     )

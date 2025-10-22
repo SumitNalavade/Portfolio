b72ba2e7f008f354cab78e9b45a030521825e986
Sumit Nalavade <vs.nalavade2003@gmail.com>
2025-10-22 17:08:47 -0500

    Dynamically underline navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import React from "react";
+import { usePathname } from 'next/navigation'
 
 const Navbar: React.FC = () => {
+    const pathname = usePathname()
+
     return (
         <div className="py-8 flex justify-end-safe items-center">
             <ul className="flex gap-x-5">
-                <li>About</li>
-                <li>Work</li>
-                <li>Blog</li>
+                <li className={`${pathname === '/' ? 'underline' : ''}`}>About</li>
+                <li className={`${pathname.includes('/work') ? 'underline' : ''}`}>Work</li>
+                <li className={`${pathname.includes('/blog') ? 'underline' : ''}`}>Blog</li>
             </ul>
         </div>
     )
